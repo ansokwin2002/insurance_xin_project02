1b266efd44083f2a1de080e71df9fe192625ea71
Your Name <ansokwin@gmail.com>
2025-12-02 09:46:55 +0700

    sdfdsf

---
+++ js/mobile-menu.js
@@ -0,0 +1,59 @@
+// ======= HAMBURGER MENU FOR MOBILE =======
+$(function () {
+  const hamburgerMenu = $("#hamburgerMenu");
+  const menuContent = $("#menuContent");
+  const menuOverlay = $("#menuOverlay");
+  const productMenu = $(".product-menu");
+
+  // Toggle mobile menu
+  hamburgerMenu.on("click", function () {
+    $(this).toggleClass("active");
+    menuContent.toggleClass("active");
+    menuOverlay.toggleClass("active");
+    $("body").toggleClass("menu-open");
+  });
+
+  // Close menu when clicking overlay
+  menuOverlay.on("click", function () {
+    hamburgerMenu.removeClass("active");
+    menuContent.removeClass("active");
+    menuOverlay.removeClass("active");
+    $("body").removeClass("menu-open");
+  });
+
+  // Close menu when clicking a menu link (except product dropdown)
+  menuContent.find("a").not(".product-menu a").on("click", function () {
+    hamburgerMenu.removeClass("active");
+    menuContent.removeClass("active");
+    menuOverlay.removeClass("active");
+    $("body").removeClass("menu-open");
+  });
+
+  // Toggle product dropdown in mobile menu
+  productMenu.find(".menu-label").on("click", function (e) {
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      productMenu.toggleClass("active");
+    }
+  });
+
+  // Close menu on window resize if desktop
+  $(window).on("resize", function () {
+    if (window.innerWidth > 768) {
+      hamburgerMenu.removeClass("active");
+      menuContent.removeClass("active");
+      menuOverlay.removeClass("active");
+      $("body").removeClass("menu-open");
+      productMenu.removeClass("active");
+    }
+  });
+
+  // Prevent body scroll when menu is open
+  $("body").on("menu-open", function () {
+    if ($("body").hasClass("menu-open")) {
+      $("body").css("overflow", "hidden");
+    } else {
+      $("body").css("overflow", "");
+    }
+  });
+});
